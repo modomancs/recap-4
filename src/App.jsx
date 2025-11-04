7b54f0a00c343e99ee3e7d7817f479c8a0bb4c8d
Efstathios <modo575@hotmail.com>
2025-11-04 11:14:12 +0100

    add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import { nanoid } from "nanoid";
-import { useState } from "react";
+
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [colors, setColor] = useState(initialColors);
+  const [colors, setColor] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
   function handleAddColor(data) {
     const newColor = { id: nanoid(), ...data };
     setColor([newColor, ...colors]);
